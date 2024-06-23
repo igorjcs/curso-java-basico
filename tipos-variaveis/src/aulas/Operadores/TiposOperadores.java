d92e7c48b93c03552a27570feffeddd1fe2d0f57
Igor Santana <igor.j.santana@outlook.com.br>
2024-06-23 12:07:37 -0300

    commit 23.06 fim Operadores

--- tipos-variaveis/src/aulas/Operadores/TiposOperadores.java
+++ tipos-variaveis/src/aulas/Operadores/TiposOperadores.java
@@ -2,4 +2,25 @@ package aulas.Operadores;
 
 public class TiposOperadores {
     
+    public static void main(String[] args) {
+       
+        // Imprimir com um if/else curto
+        int num1, num2;
+
+        num1 = 5;
+        num2 = 2;
+
+        String resultado = num1==num2 ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+
+        // Comparar Strings de forma mais efetiva
+        String nomeUm = "Igor";
+        String nomeDois = new String ("Igor");
+
+        System.out.println(nomeUm.equals(nomeDois));
+    }
+
 }
+
